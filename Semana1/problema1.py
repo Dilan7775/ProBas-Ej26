7b0d3455fb4e18cd8287aa08a2fa5bb03e9e3ea3
Dilan7775 <dilan.antonioh@uanl.edu.mx>
2026-02-23 13:33:09 -0600

    Add files via upload

---
+++ Semana1/problema1.py
@@ -0,0 +1,6 @@
+# Problema 1: Suma de 2 valores
+
+a = 5
+b = 7
+
+print(a+b)
---
+++ Semana1/problema2.py
@@ -0,0 +1,6 @@
+# Problema 2: Datos de producto
+
+nombre_producto = "Suavitel"
+precio = 45.50
+
+print(nombre_producto, precio)
